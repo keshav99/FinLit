964d55096ad81940e22910f154c69d521f4e2e2a
keshav99 <kesh.howzat@gmail.com>
2017-09-27 08:22:56 +0530

    Update 27/9

--- app/src/main/java/com/vit/finlit/finlit/MainActivity.java
+++ app/src/main/java/com/vit/finlit/finlit/MainActivity.java
@@ -1,12 +1,23 @@
 package com.vit.finlit.finlit;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import android.os.AsyncTask;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -17,13 +28,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public void getStuff(){
+        DownloadTask task = new DownloadTask();
+        task.execute("htpps://api.openweathermap.ord/data/2.5/weather?q=London,uk");
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +74,14 @@ public class MainActivity extends AppCompatActivity
         content.setAlpha(0f);
         content.animate().alpha(1f).setDuration(500);
 
+        ImageView imgFavorite = (ImageView) findViewById(R.id.donut);
+        imgFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getStuff();
+            }
+        });
+
         ListView myListView = (ListView) findViewById(R.id.listview);
         ArrayList<String> newArr = new ArrayList<String>();
         newArr.add("Hey");
@@ -46,19 +91,23 @@ public class MainActivity extends AppCompatActivity
         newArr.add("The");
         newArr.add("Isle");
 
+
+
+
         ArrayAdapter<String> newArrAd = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, newArr);
         myListView.setAdapter(newArrAd);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Dude wait I'm not programmed", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Dude wait I'm not programmed", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -66,6 +115,86 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(false);
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.setHomeAsUpIndicator(R.mipmap.bars);
+
+        toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+        navigationView.setBackground(new ColorDrawable(getResources().getColor(R.color.colorAccentFaded)));
+
+
+
+        // Create URL
+
+// Create connection
+
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpsURLConnection urlConn = null;
+
+            try{
+                url = new URL(urls[0]);
+                urlConn = (HttpsURLConnection) url.openConnection();
+                InputStream in = urlConn.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while(data!=-1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            }catch(MalformedURLException e){
+
+            }catch (IOException e){
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            try{
+                JSONObject jsonObject = new JSONObject(result);
+                String info = jsonObject.getString("weather");
+
+                Log.i("content", info);
+                JSONArray arr = new JSONArray(info);
+
+                for(int i=0; i<arr.length(); i++){
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+                    TextView text = (TextView) findViewById(R.id.textView4);
+                    text.setText(jsonPart.getString("main"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -134,3 +263,5 @@ public class MainActivity extends AppCompatActivity
     }
 
 }
+
+
